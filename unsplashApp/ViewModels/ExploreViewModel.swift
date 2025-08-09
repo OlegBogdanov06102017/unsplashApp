9dd6bbf23a33fad247a746a1459f525964810f43
Oleg <oleg.bogdanov@tabby.ai>
2025-08-10 00:12:29 +0300

    add mockData in collection view due to limited request per 1 hour

--- unsplashApp/ViewModels/ExploreViewModel.swift
+++ unsplashApp/ViewModels/ExploreViewModel.swift
@@ -8,42 +8,34 @@ final class ExploreViewModel {
     var onRandomPhotoUpdated:(() -> Void)?
     var onTopicUpdated:(([TopicForCollectionView])-> Void)?
     
-    func loadTopics() {
-        networkManager.getResponseTopic { [weak self] topic, error in
-            guard let topic = topic else { return }
-            DispatchQueue.main.async {
-                self?.topics = topic
-                self?.fetchPhotos(topics: topic)
-            }
-        }
-    }
-    
-    func fetchPhotos(topics: [Topic]) {
-        var newTopics: [TopicForCollectionView] = []
-        for topic in topics {
-            
-            networkManager.getResponsePhotoBySlug(slug: topic.slug) { photos, error in
-                guard let firstPhoto = photos?.first,
-                      let smallPhoto = firstPhoto.urls?.small else {
-                    return
-                }
-                let newTopic = TopicForCollectionView(title: topic.title, imageUrl: smallPhoto)
-                newTopics.append(newTopic)
-                
-                DispatchQueue.main.async {
-                    self.onTopicUpdated?(newTopics)
-                }
-            }
-        }
-    }
-    
-    
-    func numberOfTopicsTitle() -> Int {
-        return topics.count
-    }
-    
-    func numberOfPhotos(index: Int) -> String? {
-        return allphotosBySlug[index].urls?.small
-    }
+    //MARK: Have to comment func loadTopics() due to 50 request per 1 hour
+//    func loadTopics() {
+//        networkManager.getResponseTopic { [weak self] topic, error in
+//            guard let topic = topic else { return }
+//            DispatchQueue.main.async {
+//                self?.topics = topic
+//                self?.fetchPhotos(topics: topic)
+//            }
+//        }
+//    }
+//    
+//    func fetchPhotos(topics: [Topic]) {
+//        var newTopics: [TopicForCollectionView] = []
+//        for topic in topics {
+//            
+//            networkManager.getResponsePhotoBySlug(slug: topic.slug) { photos, error in
+//                guard let firstPhoto = photos?.first,
+//                      let smallPhoto = firstPhoto.urls?.small else {
+//                    return
+//                }
+//                let newTopic = TopicForCollectionView(title: topic.title, imageUrl: smallPhoto)
+//                newTopics.append(newTopic)
+//                
+//                DispatchQueue.main.async {
+//                    self.onTopicUpdated?(newTopics)
+//                }
+//            }
+//        }
+//    }
     
 }
